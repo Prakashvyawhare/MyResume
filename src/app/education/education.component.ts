54534425ca940ad4055c3b6482dec1a650ed78bd
Prakashvyawhare <121247686+Prakashvyawhare@users.noreply.github.com>
2023-03-07 02:48:49 +0530

    more responsive
    to make more responsive
    1. add footer
    2. change navbar
    3. change some layout

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -9,9 +9,9 @@ import { Education } from './education';
 export class EducationComponent implements OnInit {
   BE={}
   education=new Array<Education>()
-  Be = new Education("B.E.","Parvatibai Genba moze College Of Engineering","9.3 CGPA",2022,"Mechanical Engineering");
+  Be = new Education("B.E.","Parvatibai Genba moze College Of Engineering","8.41 CGPA",2022,"Mechanical Engineering");
   constructor() { 
-    let Be= new Education("B.E.","Genba Sopanroa Moze Trust's Parvatibai Genba Moze College of Engineering, Pune","9.3 CGPA",2022,"Mechanical Engineering");
+    let Be= new Education("B.E.","Genba Sopanroa Moze Trust's Parvatibai Genba Moze College of Engineering, Pune","8.41 CGPA",2022,"Mechanical Engineering");
     let Diploma= new Education("Diploma","CSMSS College of polytechnic, Aurangabad","69.47%",2018,"Mechanical Engineering")
     let X=new Education("Xth/SSC","Shri ganesh vidyalaya gajgaon, gangapur, Aurangabad","86%",2015)
     this.education.push(Be,Diploma,X)
